67e853a6f7980365361f4277199c65264826f47f
Dragonlord1005 <dragonlord100587@gmail.com>
2024-12-17 12:40:39 -0700

    Add Fastify server integration and create test page

---
+++ src/routes/test/index.tsx
@@ -0,0 +1,13 @@
+import { component$ } from '@builder.io/qwik';
+
+const TestPage = component$(() => {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            <p>This is a test page.</p>
+            <p>Hi there</p>
+        </div>
+    );
+});
+
+export default TestPage;
