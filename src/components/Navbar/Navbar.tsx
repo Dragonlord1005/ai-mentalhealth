a528d04cb8db3998b2a10002963696a4bc047bcb
Dragonlord1005 <dragonlord100587@gmail.com>
2025-02-11 18:13:28 +0000

    Add Navbar component with styles for header and logo

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+import styles from "./Navbar.module.css"
+
+export const Navbar = component$(() => {
+    return (
+        <header>
+            <p>hi</p>
+        </header>
+    );
+});
